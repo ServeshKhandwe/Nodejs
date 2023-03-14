3a4541bf5ef8e72d5218da51699091937c4870f5
Servesh Khandwe <servesh1999@gmail.com>
2023-03-14 23:37:59 +0100

    first commit and push of all the node.js files

---
+++ 4-names.js
@@ -0,0 +1,9 @@
+//Local
+const peter='peter'
+
+//Global
+const jay='jay'
+const sergio='sergio'
+
+module.exports = {jay, sergio}
+
---
+++ ahh.js
@@ -0,0 +1,9 @@
+const names = require("./4-names");
+console.log(names)
+const sayhi =require("./5-utils");
+
+
+
+sayhi('susan')
+sayhi(names.jay)
+sayhi(names.sergio)
