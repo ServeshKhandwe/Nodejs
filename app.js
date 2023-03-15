9953147632d577e51e8abe148c4e6a23ff3c0ea9
Servesh Khandwe <servesh1999@gmail.com>
2023-03-15 16:17:54 +0100

    started with learning about the OS module in NodeJs

---
+++ app.js
@@ -0,0 +1,17 @@
+const os=require('os')
+
+//info about the current user
+const user=os.userInfo()
+console.log(user)
+
+//method returns system's uptime in seconds
+console.log(`The system uptime is ${os.uptime()} seconds`)
+
+const currentOS = {
+    name: os.type(),
+    release: os.release(),
+    OStotalMemory: os.totalmem(),
+    freeMemory: os.freemem(),
+}
+
+console.log(currentOS);
