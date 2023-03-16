c305b75b5f44d3627fd93baed5eba198f24e2d07
Servesh Khandwe <servesh1999@gmail.com>
2023-03-16 21:52:51 +0100

    absolutePath

--- app.js
+++ app.js
@@ -3,3 +3,9 @@ console.log(path.sep)
 
 const filePath=path.join('/content/', 'subfolder', 'text.txt')
 console.log(filePath)
+
+const base=path.basename(filePath)
+console.log(base)
+
+const absolutePath = path.resolve(__dirname,'content','subfolder','test.txt')
+console.log(absolutePath)
