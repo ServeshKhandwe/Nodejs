70c3a3c0823da8d1619d8b6e1687654611cea3de
Servesh Khandwe <servesh1999@gmail.com>
2023-03-16 16:51:00 +0100

    filePath of path module

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const path = require('path')
 console.log(path.sep)
 
-const filePath=path.join('/content', 'subfolder', 'text.txt')
+const filePath=path.join('/content/', 'subfolder', 'text.txt')
 console.log(filePath)
