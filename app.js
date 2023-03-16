87e7262665c7aba311447c57ca7eeab937e97f78
Servesh Khandwe <servesh1999@gmail.com>
2023-03-16 22:00:26 +0100

    fs-sync

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-const path = require('path')
-console.log(path.sep)
+const fs=require('fs')
+const {readFileSync, writeFileSync} = require('fs')
 
-const filePath=path.join('/content/', 'subfolder', 'text.txt')
-console.log(filePath)
-
-const base=path.basename(filePath)
-console.log(base)
-
-const absolutePath = path.resolve(__dirname,'content','subfolder','test.txt')
-console.log(absolutePath)
