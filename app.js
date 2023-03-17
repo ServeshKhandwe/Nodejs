4cbc5e7f2f383b90539ae54f3b968bee498b7781
Servesh Khandwe <servesh1999@gmail.com>
2023-03-17 13:33:12 +0100

    readFileSync

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 const fs=require('fs')
 const {readFileSync, writeFileSync} = require('fs')
 
+const first = readFileSync('./content/first.txt','utf-8')
+const second = readFileSync('./content/second.txt','utf-8')
+ 
