05b1d93a653737b40121d3a53759e50ace7a1f44
Servesh Khandwe <servesh1999@gmail.com>
2023-03-21 16:58:00 +0100

    fs-async started

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-const fs=require('fs')
-const {readFileSync, writeFileSync} = require('fs')
+const { readFile, writeFile } = require('fs')
 
-const first = readFileSync('./content/first.txt','utf-8')
-const second = readFileSync('./content/second.txt','utf-8')
-
-console.log(first)
-console.log(second)
-
-writeFileSync('./content/result-sync.txt', `Here is the result: ${first}, ${second}`,  {flag: 'a'})  
+readFile('./content/first.txt','utf-8',(err, result)=>{
+     if(err){
+        console.log(err)
+        return
+     }
+     console.log(result)
+})
